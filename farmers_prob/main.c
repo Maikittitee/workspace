c596a4be4f5b9de62096c5ae3467a8d8fbd4fa32
ktunchar <maikittitee1615@gmail.com>
2022-08-28 23:16:03 +0700

    farmer

---
+++ farmers_prob/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+char	boat(char *c)
+{
+	c = 'R';
+	return (c);
+}
+
+int main()
+{
+    //  is Right side and 0 is Left side
+    char	farmerloc;
+    char	wolfloc;
+    char    cabageloc;
+	char	goatloc;
+
+	int check;
+
+	check = 0;
+	farmerloc = 'L';
+	wolfloc = 'L';
+	cabageloc = 'L';
+	goatloc = 'L';
+
+	while (!check)
+	{
+	
+		if (farmerloc = 'R' &&  wolfloc 'R' && cabageloc = 'R' && goatloc = 'R')
+			check = 1;
+	}
+}
