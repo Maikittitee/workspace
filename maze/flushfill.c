70aff582d4e33f180f75cbc95df1d158ea2f236d
Krittithee Tuncharoen <maikittitee@Krittithees-MacBook-Pro.local>
2022-10-15 23:52:36 +0700

    fix

--- maze/flushfill.c
+++ maze/flushfill.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-//Segment fault occured by can_go
+//TODO >> Segment fault occured by can_go
 
 int size_x;
 int size_y;
 
-//int	size_x, size_y;
-
-void	show_maze(int maze[size_y][size_x])
+void	show_maze(int maze[size_y + 1][size_x + 1])
 {
 	for (int b = 0; b <= size_y; b++)
 	{
@@ -17,29 +15,29 @@ void	show_maze(int maze[size_y][size_x])
 		printf("\n");
 	}
 }
-int	can_go(int x,int y, int maze[size_y][size_x])
+int	can_go(int x,int y, int maze[size_y + 1][size_x + 1])
 {
-	if (x >= 0 && y >= 0 && x < size_x && y < size_y && maze[y][x] == 0)
+	if (x >= 0 && y >= 0 && x <= size_x - 1 && y <= size_y - 1 && maze[y][x] == 0)
 		return (1);
 	else
 		return (0);
 }
 
-int	ft_flushfill(int x, int y, int maze[size_y][size_x])
+int	ft_flushfill(int x, int y, int maze[size_y + 1][size_x + 1])
 {
-	if (x == size_x - 1 && y == size_y - 1 && can_go(x,y,maze))
+	if (x == size_x - 1 && y == size_y - 1)
 		return (1);
 
-	if (can_go(x,y, maze)/* && maze[x][y]*/)
+	if (can_go(x,y, maze))
 	{
-		if (y < size_y - 1 && ft_flushfill(x, y + 1,maze))
+		if (ft_flushfill(x, y + 1,maze))
 			return (1);
-		if (x < size_x -1 && ft_flushfill(x + 1, y, maze))
+		if (ft_flushfill(x + 1, y, maze))
 			return (1);
-		if (y > 0 && ft_flushfill(x, y - 1,maze))
-		 	return (1);
-		if (x > 0 && ft_flushfill(x - 1,y , maze))
-			return (1);
+		// if (ft_flushfill(x, y - 1,maze))
+		//  	return (1);
+		// if (ft_flushfill(x - 1,y , maze))
+		// 	return (1);
 	}
 	return (0);
 }
@@ -58,19 +56,19 @@ int	main()
 			scanf("%d",&maze[i][j]);
 		}
 	}
-	printf("after input show -----\n");
+	//add frame of maze (prevent segmentation fault from some case)
 	for (int c = 0; c <= size_y; c++)
 	{
 		for (int d = 0; d <= size_x; d++)
 		{
-			printf(" %d ",maze[c][d]);
 			if (d == size_x || c == size_y)
-				maze[c][d] = 8;
+				maze[c][d] = 1;
 		}
-		printf("\n");
 	}
-	printf("function show ----- \n");
+	//show maze for making sure that maze was correct;
 	show_maze(maze);
+
+	//check result from flushfill function
 	if (ft_flushfill(0,0,maze))
 		printf("YESSSS\n");
 	else
